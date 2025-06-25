abefd25fdf11de9bb1aa33b80aea64a2a1b0e2ee
CHETAN B R <chiranjeevichetan1996@gmail.com>
2025-06-25 15:31:49 +0530

    Updated app version

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Parrot Analyzer",
   slug: "parraotanalyzer",
-  version: "1.2.0",
+  version: "1.2.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "myapp",
